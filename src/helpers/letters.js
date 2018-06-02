c701cbca1031643f24c8fec434117640f1faee8a
Pete Schuster <pas5027@gmail.com>
2018-06-02 15:38:58 -0400

    all letters

--- src/helpers/letters.js
+++ src/helpers/letters.js
@@ -1,2 +1,2 @@
-// export const LIST = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-export const LIST = ['a', 'b', 'c'];
+export const LIST = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+// export const LIST = ['a', 'b', 'c'];
